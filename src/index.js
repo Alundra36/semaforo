1aa685340ae9ce8cd338c7f0a760cb8ac4f17eed
Juan Manuel de Palacios <juanma36@gmail.com>
2019-10-02 10:03:40 -0300

    my-semaforo

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import TrafficLight from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(<TrafficLight />, document.getElementById("root"));
+
+serviceWorker.unregister();
+
